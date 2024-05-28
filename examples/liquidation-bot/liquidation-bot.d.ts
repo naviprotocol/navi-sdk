f19a58db25098f14944ad10ad073cba17587a9d2
Luren L. <shuhr@usc.edu>
2024-05-28 14:56:15 -0400

    Revert "update"
    This reverts commit 08ff4bee26e7e6bc3eb88b3f62bf859719eec3de.

--- examples/liquidation-bot/liquidation-bot.d.ts
+++
@@ -1,3 +0,0 @@
-import { CoinInfo } from 'navi-sdk/dist/types';
-import { AccountManager } from "navi-sdk/dist/libs/AccountManager";
-export declare function getCoinObj(account: AccountManager, realCoin: CoinInfo): Promise<any[]>;
