a49beb2d97e6e1d813fcd62ea617d0e73fcb45b8
Luren L. <haoran.sde@gmail.com>
2025-02-05 10:35:48 -0500

    Update index.ts

--- src/libs/AccountManager/index.ts
+++ src/libs/AccountManager/index.ts
@@ -389,10 +389,13 @@ export class AccountManager {
       await depositCoinWithAccountCap(txb, poolConfig, toDeposit, accountCapAddress);
     } else {
       const mergedCoinObject = returnMergedCoins(txb, coinInfo);
+      const mergedCoinObjectWithAmount = txb.splitCoins(mergedCoinObject, [
+        amount,
+      ]);
       await depositCoinWithAccountCap(
         txb,
         poolConfig,
-        mergedCoinObject,
+        mergedCoinObjectWithAmount,
         accountCapAddress
       );
     }
@@ -517,7 +520,10 @@ export class AccountManager {
       await repayDebt(txb, poolConfig, toDeposit, repayAmount);
     } else {
       const mergedCoinObject = returnMergedCoins(txb, coinInfo);
-      await repayDebt(txb, poolConfig, mergedCoinObject, repayAmount);
+      const mergedCoinObjectWithAmount = txb.splitCoins(mergedCoinObject, [
+        repayAmount,
+      ]);
+      await repayDebt(txb, poolConfig, mergedCoinObjectWithAmount, repayAmount);
     }
 
     const result = SignAndSubmitTXB(txb, this.client, this.keypair);
