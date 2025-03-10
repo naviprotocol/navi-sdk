881898ea35650ac9b0c1f1a2f7c64b4610df87ff
Jin-K-Yang <ken8989580@gmail.com>
2025-03-10 11:10:19 +0000

    update: swap ptb not yet

--- src/libs/Aggregator/index.ts
+++ src/libs/Aggregator/index.ts
@@ -1,4 +1,11 @@
-export * from './getQuote';
-export * from './config';
-export * from './swapPTB';
-export * from './utils';
+export * from "./getQuote";
+export * from "./config";
+export * from "./swapPTB";
+export * from "./utils";
+
+// import { buildSwapPTBFromQuote } from "./swapPTB";
+// async function main() {
+//   await buildSwapPTBFromQuote();
+// }
+
+// main();
