e2a97f8e5d70c65a8fec45e1b894813e455f9ba6
xiaodi007 <334830452@qq.com>
2025-05-09 09:59:13 +0800

    change vSui config to v2

--- src/address.ts
+++ src/address.ts
@@ -650,10 +650,9 @@ export const WAL: CoinInfo = {
 };
 
 export const vSuiConfig = {
-  // TODO
   ProtocolPackage:
-    "0x549e8b69270defbfafd4f94e17ec44cdbdd99820b33bda2278dea3b9a32d3f55",
-  pool: "0x7fa2faa111b8c65bea48a23049bfd81ca8f971a262d981dcd9a17c3825cb5baf",
+    "0x68d22cf8bdbcd11ecba1e094922873e4080d4d11133e2443fddda0bfd11dae20",
+  pool: "0x2d914e23d82fedef1b5f56a32d5c64bdcc3087ccfea2b4d6ea51a71f587840e5",
   metadata:
     "0x680cd26af32b2bde8d3361e804c53ec1d1cfe24c7f039eb7f549e8dfde389a60",
   wrapper: "0x05",
