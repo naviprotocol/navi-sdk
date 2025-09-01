2fa4337c771fd9dd38bb424e019189e2742afd31
Haozhe Zhang <zhz19980514@gmail.com>
2025-08-31 23:46:25 -0700

    fix: fix decimals

--- src/libs/Aggregator/buildSwapWithoutServiceFee.ts
+++ src/libs/Aggregator/buildSwapWithoutServiceFee.ts
@@ -295,8 +295,8 @@ export async function buildSwapWithoutServiceFee(
 
   txb.transferObjects([coinIn], userAddress);
   const amountInValue = quote.from_token
-  ? ((Number(quote.amount_in) * (quote.from_token.price ?? 0)) / quote.from_token?.decimals) * 1e9
-  : 1e15
+    ? ((Number(quote.amount_in) / Math.pow(10, quote.from_token.decimals)) * (quote.from_token.price ?? 0)) * 1e9
+    : 1e15
   const shouldEnablePositiveSlippage =
     !disablePositiveSlippage && quote.is_accurate === true && amountInValue !== 0
 
